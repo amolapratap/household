b21fd1613b06d8737c53dc823788ab82d77bff0b
amol-pratap <23f3001602@ds.study.iitm.ac.in>
2024-11-06 21:49:34 +0530

    aaddedon 6/11/2024

---
+++ app.py
@@ -0,0 +1,13 @@
+#starting of the app
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def Home():
+    return render_template("login.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
